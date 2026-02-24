638a3a725c5dcd0ecbe4673d1495f0c3e3a9fe87
Human-ctrl-ai <dassuvra336@gmail.com>
2026-02-24 23:46:25 +0530

    Add files via upload

---
+++ HJJ.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int n;
+    cin >> n;
+    for(int i=2; i<=3; i++){
+        n += 10;
+    }
+    cout << n;
+    return 0;
+}
---
+++ SKONG.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int d, h;
+    cin >> d >> h;
+    cout << ((d-4)*24) + h;
+    return 0;
+}
