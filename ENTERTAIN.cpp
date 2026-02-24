b1378f6dc47269cbf4e7fe657282512c6ca8709d
Human-ctrl-ai <dassuvra336@gmail.com>
2026-02-24 23:44:52 +0530

    Add files via upload

---
+++ ENTERTAIN.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n;
+	cin >> n;
+	if (n*200 > 1000){
+	    cout << 1000;
+	} else{
+    	cout << n*200;
+	}
+	return 0;
+}
